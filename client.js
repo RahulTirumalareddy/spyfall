b3489c19179d2711eb30ac489f08b1fff17d1c82
Rahul Tirumalareddy <rahul@Rahuls-MacBook-Pro.local>
2018-01-18 10:02:33 -0800

    correctly implemented leaving lobby

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const server = new WebSocket('ws://127.0.0.1:8080');
 var username;
 var users;
+var numberInLobby;
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('username_input').onkeydown = function(e){
@@ -19,17 +20,23 @@ server.addEventListener('message', function (event) {
     if (message.startsWith('initialUsers')) {
       users = JSON.parse(message.substring(12));
       users.forEach(addToUserList);
+      numberInLobby = users.length;
+      changeNumberInLobby(numberInLobby);
     }
 
     if (message.startsWith('newUser')) {
       let newUser = message.substring(7);
       users.push(newUser);
       addToUserList(newUser);
+      changeNumberInLobby(numberInLobby+1);
     }
 
     if (message.startsWith('delete')) {
       let deletedUser = message.substring(6);
+      let index = users.indexOf(deletedUser);
+      users.splice(index,1);
       removeFromUserList(deletedUser);
+      changeNumberInLobby(numberInLobby-1);
     }
 });
 
@@ -42,25 +49,37 @@ function addToUserList(user) {
 }
 
 function removeFromUserList(user) {
-  if (user){
-    let removedNode = document.getElementById(user);
-    removedNode.parentNode.removeChild(removedNode);
-  }
+  let removedNode = document.getElementById(user);
+  removedNode.parentNode.removeChild(removedNode);
 }
 
 window.onbeforeunload = function(e) {
-  server.send("delete" + username);
+  if (username) {
+    server.send("delete" + username);
+  }
   server.close();
 };
 
 function submitUsername() {
   let username_input = document.getElementById("username_input");
-  if (!users.includes(username_input.value)) {
+  let input = username_input.value
+  if (!users.includes(input) && input.length > 0) {
     server.send('newUser' + username_input.value);
     username = username_input.value;
     username_input.parentNode.removeChild(username_input);
+    let button = document.getElementById('join_button');
+    button.parentNode.removeChild(button);
     document.getElementById('my_username').innerHTML = "You are " + username;
   } else {
-    alert('Username already exists with that name!');
+    if (input.length <= 0) {
+      alert('Username must be at least one character!');
+    } else {
+      alert('Username already exists with that name!')
+    }
   }
 };
+
+function changeNumberInLobby(newNumber) {
+  numberInLobby = newNumber;
+  document.getElementById('numberInLobby').innerHTML = numberInLobby;
+}
